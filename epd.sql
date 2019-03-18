973c94005960d2ad3740fbe00aae15653366bad8
fuyitaoqqq <48234109+fuyitaoqqq@users.noreply.github.com>
2019-03-18 09:53:00 +0800

    add epd.sql

---
+++ epd.sql
@@ -0,0 +1,92 @@
+-- MySQL dump 10.17  Distrib 10.3.13-MariaDB, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: epd
+-- ------------------------------------------------------
+-- Server version	5.7.23-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `api`
+--
+
+DROP TABLE IF EXISTS `api`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `api` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uri` varchar(255) NOT NULL COMMENT '接口uri',
+  `description` varchar(255) DEFAULT NULL COMMENT '接口描述',
+  `method` varchar(100) NOT NULL COMMENT '接口请求方式',
+  `request_header` blob COMMENT '请求头',
+  `request_success` blob COMMENT '成功返回',
+  `request_fail` blob COMMENT '失败返回',
+  `return_success_json` blob COMMENT '成功返回json',
+  `return_fail_json` blob COMMENT '失败返回json',
+  `project_id` int(11) NOT NULL,
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `module_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `api_id_uindex` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='接口详情';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `module`
+--
+
+DROP TABLE IF EXISTS `module`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `module` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL COMMENT '模块名称',
+  `api_list` blob COMMENT '模块下的uri接口列表',
+  `project_id` int(11) NOT NULL COMMENT '所属项目ID',
+  `create_time` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `module_id_uindex` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='项目模块';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `project`
+--
+
+DROP TABLE IF EXISTS `project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL COMMENT '项目名称',
+  `description` varchar(255) DEFAULT NULL COMMENT '项目说明',
+  `create_time` datetime NOT NULL COMMENT '项目创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '项目信息更新时间',
+  `request_header` blob COMMENT '项目全局参数：请求头',
+  `request_success` blob COMMENT '项目全局参数：成功返回参数',
+  `request_fail` blob COMMENT '项目全局参数：失败返回参数',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `project_id_uindex` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='项目实体类';
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-03-18  9:51:43
